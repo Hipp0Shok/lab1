069db51b3768b6d3c1f2556d95a7b4ef41d300be
Hipp0Shok <gleb.zhivotov@mail.ru>
2018-11-10 21:21:39 +0300

    Fucking doesn't work

--- dish.cpp
+++ dish.cpp
@@ -1,57 +1,6 @@
 #include "dish.h"
 
 Dish::Dish()
-    :  Base()
-{
-}
-Dish::Dish(std::string const  &name, float const fats, float  const proteins, float  const carbohydrates,
-           float  const organicAcids, float  const alimentaryFibers, float  const weight)
-    :  Base(name, fats, proteins, carbohydrates, organicAcids, alimentaryFibers, weight)
-{
-}
-Dish::Dish(const Dish &existing):
-    Base(existing)
 {
 
-}
-Dish::~Dish()
-{
-
-}
-
-void Dish::changeFats(float fats)
-{
-    if(fats >= 0 && fats <= 100)
-    {
-        _fats = fats;
-    }
-}
-
-void Dish::changeProteins(const float proteins)
-{
-    if(proteins >= 0 && proteins <= 100)
-    {
-        _proteins = proteins;
-    }
-}
-void Dish::changeAlimentaryFibers(const float alimentaryFibers)
-{
-    if(alimentaryFibers >= 0 && alimentaryFibers <= 100)
-    {
-        _alimentaryFibers = alimentaryFibers;
-    }
-}
-void Dish::changeCarbohydrates(const float carbohydrates)
-{
-    if(carbohydrates >= 0 && carbohydrates <= 100)
-    {
-        _carbohydrates = carbohydrates;
-    }
-}
-void Dish::changeOrganicAcids(const float organicAcids)
-{
-    if(organicAcids >= 0 && organicAcids <= 100)
-    {
-        _organicAcids = organicAcids;
-    }
 }
--- dish.h
+++ dish.h
@@ -1,23 +1,11 @@
 #ifndef DISH_H
 #define DISH_H
-#include "base.h"
-#include <iostream>
-#include <string>
 
 
-class Dish: public Base
+class Dish
 {
 public:
     Dish();
-    Dish(const std::string &name, const float fats, const float proteins, const float carbohydrates, const float organicAcids, const float alimentaryFibers, const float weight);
-    Dish(const Dish &);
-    ~Dish();
-    void changeFats(float const fats);
-    void changeProteins(float const proteins);
-    void changeCarbohydrates(float  const carbohydrates);
-    void changeAlimentaryFibers(float const alimentaryFibers);
-    void changeOrganicAcids(float  constorganicAcids);
-
 };
 
 #endif // DISH_H
